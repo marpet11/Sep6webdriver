14450d3e8193f339bf6b67dc79a0041137fd79ea
marpet11 <mariayachnaya@gmail.com>
2021-11-07 13:29:26 -0600

    InterviewQuestions

--- src/test/java/InterviewQuestions.java
+++ src/test/java/InterviewQuestions.java
@@ -16,5 +16,11 @@ public class InterviewQuestions {
         }
     }
 
+    @Test
+    public void TestHelloWorldEvenNumbers (){
+        for (int i = 1; i <= 10; i++) {
+            if (i%2 == 0) System.out.println("Hello World");
+        }
+    }
 
 }
