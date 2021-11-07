01f6af6af768716f539dce62ef630048de5803a4
marpet11 <mariayachnaya@gmail.com>
2021-11-07 13:08:56 -0600

    InterviewQuestions

---
+++ src/test/java/InterviewQuestions.java
@@ -0,0 +1,20 @@
+import org.testng.annotations.Test;
+
+public class InterviewQuestions {
+
+    @Test
+    public void TestEvenNumbers (){
+        for (int i = 1; i <= 10; i++) {
+            if (i%2 == 0) System.out.println(i);
+        }
+    }
+
+    @Test
+    public void testEvenNumbers2option() {
+        for (int i = 2; i <=10; i = i + 2) {
+            System.out.println(i);
+        }
+    }
+
+
+}
