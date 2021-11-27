2b2c2c1a20a7acf13340db931be4819f8a539368
marpet11 <mariayachnaya@gmail.com>
2021-11-27 15:56:26 -0600

    InterviewQuestions

--- src/test/java/InterviewQuestions.java
+++ src/test/java/InterviewQuestions.java
@@ -1,5 +1,7 @@
 import org.testng.annotations.Test;
 
+import java.util.Arrays;
+
 public class InterviewQuestions {
 
     @Test
@@ -17,10 +19,40 @@ public class InterviewQuestions {
     }
 
     @Test
-    public void TestHelloWorldEvenNumbers (){
+    public void TestHelloWorldEvenNumbers() {
         for (int i = 1; i <= 10; i++) {
-            if (i%2 == 0) System.out.println("Hello World");
+            if (i % 2 == 0) System.out.println("Hello World");
         }
     }
+
+    @Test
+    public void testSwap2NumbersInArray() {
+        int[] array = {1, 2, 3, 4}; // data we have
+        solution(array, 0, 3); // newly created method below
+        System.out.println(Arrays.toString(array)); // result
+    }
+
+    private void solution(int[] array, int firstNumber, int secondNumber) {
+        int temporary = firstNumber; // this is only to save the value of the first number as it will be overwritten.
+        array[firstNumber] = array[secondNumber];
+        array[secondNumber] = temporary;
+    }
+
+    @Test
+    public void testCountLetters() {
+        String Input = "Hello World";
+        char letterToFind = 'l';
+        int AmountOfLetters = solutionforletter(Input, letterToFind);
+        System.out.println(AmountOfLetters);
+    }
+
+    private int solutionforletter(String Input, char letterToFind) {
+        int result = 0;
+        for (char eachChar : Input.toCharArray()){
+            if (eachChar== letterToFind) result++;
+        }
+        return result;
+    }
+
 
 }
